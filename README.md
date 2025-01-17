5ab742e1876aac6cbea86811ae6ec3ca57811684
unknown <sunebarikara@gmail.com>
2025-01-17 13:15:07 +0100

    Make changes to the current one

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ example 'I am a good boy' in English and in jungle language it is '3 1ma 1 ga44d
 HOW TO USE IT:
  Simply type English language and click enter it will convert it to Jungle language 
 '''
+
+
+
+
+### Jungle Converter
--- jungle.py
+++ jungle.py
@@ -1,44 +1,33 @@
-words = input("input language: ")
-language = ""
-converter = {"a": "1",
-"e": "2", "i": "3", "o": "4",
-"u": "5", "A": "1", "E": "2",
-"I": "3", "O": "4", "U": "5",
-'q': "qa", "w": "wa", "r": 'ra',
-'t': "ta", "y": "ya", "p": "pa",
-"s": "sa", "d": "da", "f": "fa",
-"g": "ga", "h": "ha", "j": "ja",
-"k": "ka", 'l': 'la', "z": "za",
-"x": "xa", "c": "ca", "v": "va",
-"b": "ba", "n": "na", "m": "ma",
-"Q": "Qa", "W": "Wa", "R": "Ra",
-"T": "Ta", "Y": "Ya", "P": 'Pa',
-"S": "Sa", "D": "Da", "F": "Fa",
-"G": "Ga", "H": "Ha", "J": "Ja",
-"K": "Ka", "L": "La", "Z": "Za",
-"X": "Xa", "C": "Ca", "V": "Va",
-"B": "Ba", "N": "Na", "M": "Ma"}
-translator = {"1": "a", "2": "e",
-"3": "i", "4": "o", "5": "u"}
+def get_language():
+    return input("Input language (jungle/english): ").strip().lower()
 
+def translation(words, language):
+    converter = {
+        "a": "1", "e": "2", "i": "3", "o": "4", "u": "5", "A": "1", "E": "2", "I": "3", "O": "4", "U": "5",
+        'q': "qa", "w": "wa", "r": 'ra', 't': "ta", "y": "ya", "p": "pa", 's': "sa", "d": "da", "f": "fa",
+        "g": "ga", "h": "ha", "j": "ja", "k": "ka", 'l': 'la', "z": "za", "x": "xa", "c": "ca", "v": "va",
+        "b": "ba", "n": "na", "m": "ma", "Q": "Qa", "W": "Wa", "R": "Ra", "T": "Ta", "Y": "Ya", "P": 'Pa',
+        "S": "Sa", "D": "Da", "F": "Fa", "G": "Ga", "H": "Ha", "J": "Ja", "K": "Ka", "L": "La", "Z": "Za",
+        "X": "Xa", "C": "Ca", "V": "Va", "B": "Ba", "N": "Na", "M": "Ma"
+    }
+    translator = {"1": "a", "2": "e", "3": "i", "4": "o", "5": "u"}
 
-def translation():
     output = ""
-    if language.lower() == "jungle":
+    if language == "jungle":
         for word in words:
-            if word == "a":
-                word = ""
-            elif word == "A":
+            if word in ["a", "A"]:
                 word = ""
             output += translator.get(word, word)
-    elif language.lower() == "english":
+    elif language == "english":
         for word in words:
             output += converter.get(word, word)
     return output
 
-for word in words:
-    if word in "12345":
-        language = "jungle"
-    else:
-        language = "english"
-print(translation())
+def main():
+    words = input("Input words: ")
+    language = get_language()
+    translated_words = translation(words, language)
+    print("Translated words:", translated_words)
+
+if __name__ == "__main__":
+    main()
